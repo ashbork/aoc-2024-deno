23ca8a7a31c34696c8f5eb3a0b1c0f115a726937
Ash <maybork@pm.me>
2024-12-03 16:15:45 +0100

    add parameterize function

---
+++ common/test.ts
@@ -0,0 +1,27 @@
+import { assert } from "@std/assert";
+
+type Output<T extends readonly unknown[], V> = {
+  readonly [P in keyof T]: V;
+};
+
+/**
+ * Takes all of the items of `input` and runs them through `func`, `assert`ing that
+ * they are equal to their expected outputs. Similar to `pytest.mark.parameterize`.
+ *
+ * @param input a tuple of inputs (`as const` to keep `output` in line)
+ * @param output a tuple of outputs
+ * @param func function mapping from the input to the output.
+ */
+export const parameterize = <Input extends readonly unknown[], Value>(
+  input: Input,
+  output: Output<Input, Value>,
+  func: (input: Input[number]) => Value
+): void => {
+  input.forEach((inputValue, idx) => {
+    const actual = func(inputValue);
+    assert(
+      actual === output[idx],
+      `Parameterize failed on run ${idx} - expected ${output[idx]}, got ${actual}`
+    );
+  });
+};
