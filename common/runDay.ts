7b2c4ff5129d20c4b148ba4976ce195d217744d6
Ash <maybork@pm.me>
2024-12-07 19:25:59 +0100

    add time metrics for days

--- common/runDay.ts
+++ common/runDay.ts
@@ -1,5 +1,23 @@
 import { join } from "@std/path/join";
 
+const runWithMeasuredTime = <T>(cb: () => T): [T, number] => {
+  const t0 = performance.now();
+  const result = cb();
+  const tdelta = performance.now() - t0;
+
+  return [result, tdelta];
+};
+
+const formatTime = (millis: number) => {
+  if (millis >= 100) {
+    return (millis / 100).toPrecision(2).padStart(3) + "s";
+  }
+  if (millis < 1) {
+    return (millis * 100).toPrecision(2).padStart(3) + "µs";
+  }
+  return millis.toPrecision(2).padStart(3) + "ms";
+};
+
 const rejectWithError = (error?: string) => {
   if (error) {
     console.error(error);
@@ -21,21 +39,31 @@ export const runDay = async (
     input = await Deno.readTextFile(path);
   } catch {
     return rejectWithError(
-      `❌ Failed to read file: ${path}. You might be missing input.txt - it's not in the Git repo per the author's wishes.`
+      `❌ Failed to read file: ${path}. You might be missing input.txt - it's not in the Git repo per the author's wishes. Get it from https://adventofcode.com/2024/day/${day}/input`
     );
   }
 
   try {
-    console.log(`⭐️   ${oneStarCallback(input)}`);
+    const [result, time] = runWithMeasuredTime(() => oneStarCallback(input));
+    console.log(
+      `⭐️   ${String(result).padEnd(20)} %c(${formatTime(time)})`,
+      time < 100 ? "color: grey" : "color: red"
+    );
   } catch (e) {
     return rejectWithError(`❌ Error running the ⭐️:\n${e}`);
   }
 
   if (!twoStarCallback) {
-    return console.log("⭐️⭐️ not done");
+    return console.log("%c⭐️⭐️ not done", "color: grey");
   }
   try {
-    console.log(`⭐️⭐️ ${twoStarCallback(input)}`);
+    const [result, time] = runWithMeasuredTime(() => twoStarCallback(input));
+
+    console.log(
+      `%c⭐️⭐️ ${String(result).padEnd(20)} %c(${formatTime(time)})`,
+      "color: yellow",
+      "color: grey"
+    );
   } catch (e) {
     return rejectWithError(`❌ Error running the ⭐️⭐️:\n${e}`);
   }
